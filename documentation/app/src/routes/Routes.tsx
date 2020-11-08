c124d1fda2dd0aa9fdcfc42c5d9b431febd5031a
Dein Budda <reschan12345@gmail.com>
2020-11-08 15:52:26 +0100

    rush format

--- documentation/app/src/routes/Routes.tsx
+++ documentation/app/src/routes/Routes.tsx
@@ -48,7 +48,7 @@ export function useRoutes() {
     });
   }
 
-  getBreadcrumbName(route:)
+  // getBreadcrumbName(route:)
 
   return {
     routes,
--- packages/react-datagrid-mui/src/__tests__/DataGridMui.test.tsx
+++ packages/react-datagrid-mui/src/__tests__/DataGridMui.test.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import {DataGridPlain} from "@mvp-react/react-datagrid-plain";
-import {datagridMuiTheme} from "../";
+import { DataGridPlain } from '@mvp-react/react-datagrid-plain';
+import { datagridMuiTheme } from '../';
 
 it('should render', () => {
   shallow(
-    <DataGridPlain {...datagridMuiTheme} colDef={[]} onLoadData={() => new Promise((res) => res())} />
+    <DataGridPlain
+      {...datagridMuiTheme}
+      colDef={[]}
+      onLoadData={() => new Promise((res) => res())}
+    />
   );
 });
